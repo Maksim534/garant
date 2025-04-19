af3ae5daa5b8408c264ed91d5622e77b7120ef98
Maksim534 <129356035+Maksim534@users.noreply.github.com>
2025-04-19 17:48:50 +0300

    Delete middlewares directory

--- middlewares/__init__.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Dispatcher
-
-from loader import vip
-from .throttling import ThrottlingMiddleware
-
-
-if __name__ == "middlewares":
-    vip.middleware.setup(ThrottlingMiddleware())
